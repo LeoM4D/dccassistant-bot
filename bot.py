665928317f569544861c86c21e8fe50e8fac27e0
LeoM4D <singaraja4dofficial@gmail.com>
2025-04-28 10:40:42 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -42,7 +42,3 @@ def run_telegram_bot():
 if __name__ == "__main__":
     # Start Telegram bot in background thread
     threading.Thread(target=run_telegram_bot, daemon=True).start()
-
-    # Start Flask app
-    app.run(host="0.0.0.0", port=8080)
-
